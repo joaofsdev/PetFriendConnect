7b687ebf41956fc846e6d8c9a3c676b5f37450ac
João Francisco da Silva <167267041+joaofsdev@users.noreply.github.com>
2026-03-23 18:20:32 -0300

    Delete backend directory

--- backend/src/middlewares/admin.js
+++
@@ -1,8 +0,0 @@
-function admin(req, res, next) {
-  if (req.usuario.tipo !== 'ADMIN') {
-    return res.status(403).json({ error: 'Acesso negado. Permissão de administrador necessária.' });
-  }
-  next();
-}
-
-module.exports = admin;
